22703c81fbf3be76f6cc07f287dbb1364aa8dd00
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-10-18 14:13:35 -0600

    refactor: 💡 Initial tw configuration and providers setup

---
+++ apps/web/app/providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+/**
+ * Provides context providers for the app.
+ * @param children - The child components to be wrapped by the providers.
+ * @param themeProps - The theme provider props.
+ * @returns The JSX element containing the wrapped child components.
+ */
+
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+//import { UserProvider } from "@auth0/nextjs-auth0/client";
+
+export interface ProvidersProps {
+  children: JSX.Element | JSX.Element[];
+  themeProps?: ThemeProviderProps;
+}
+
+export function Providers({
+  children,
+  themeProps,
+}: ProvidersProps): JSX.Element {
+  return (
+    //<UserProvider>
+    <NextUIProvider>
+      <NextThemesProvider {...themeProps} enableSystem>
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+    //</UserProvider>
+  );
+}
