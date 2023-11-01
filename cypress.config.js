de715c68efd52c4d5179eb44f57134b58c3e40cf
rodrigo rodriguez de luna <a01384318@itesm.mx>
2023-11-01 14:41:52 -0600

    fix: 🐛 fix secret leak
    fix secret leak

--- cypress.config.js
+++ cypress.config.js
@@ -8,12 +8,7 @@ module.exports = defineConfig({
       on("task", {
         async connectDB(query){
           const client = new Client({
-            user: "william-monroy",
-            password: "FE90vBORUCgp",
-            host: "ep-broken-salad-00817204.us-east-2.aws.neon.tech",
-            database: "neondb",
-            ssl: true,
-            port: 5432
+      
           })
           await client.connect()
           const res = await client.query(query)
