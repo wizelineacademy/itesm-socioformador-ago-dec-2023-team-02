4d909865ca80162309c676dd3556173a0778d9c6
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-11-15 09:41:16 -0600

    style: 💄 Styled integration of sidebar and dashboard

--- apps/web/app/(root)/admin/layout.tsx
+++ apps/web/app/(root)/admin/layout.tsx
@@ -1,15 +1,30 @@
 import GroupSidebar from "@/components/admin/groupSidebar/organisms/group-sidebar";
 import { GroupsContextProvider } from "@/context/groups-context";
 
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+// Define the Inter font with the "latin" subset
+const inter = Inter({ subsets: ["latin"] });
+
+// Define the metadata for the page
+export const metadata: Metadata = {
+    title: "WizePrompt",
+    description: "",
+};
+
+
 export default function AdminRootLayout({children}: {children: React.ReactNode;}): any {  
   return (
+    <body className={inter.className}>
     <GroupsContextProvider>
         <div className="flex flex-row">
           <GroupSidebar/>
-          <section>
+          <section className="w-full h-full">
             {children}
           </section>
         </div>
     </GroupsContextProvider>
+    </body>
   );
 }
