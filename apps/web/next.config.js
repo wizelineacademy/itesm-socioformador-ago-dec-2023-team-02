3d064b1ff02b1d89e49b670b793ed44d5112f268
María Eugenia Ontiveros Bellés <a01197661@itesm.mx>
2023-11-17 07:41:54 -0600

    fix: 🐛 Added headers to config file to fix vulnerabilities

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://lh3.googleusercontent.com/',
+          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+          // },
+        ],
+      },
+    ];
+  },
   reactStrictMode: false,
   transpilePackages: ["ui"],
   images: {
@@ -10,6 +31,7 @@ const nextConfig = {
       "lh3.googleusercontent.com",
     ],
   },
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
