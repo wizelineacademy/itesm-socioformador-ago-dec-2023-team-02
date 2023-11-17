18b3a72f80908706157c60163f20ca4921f756e2
María Eugenia Ontiveros Bellés <a01197661@itesm.mx>
2023-11-17 11:11:48 -0600

    fix: 🐛 Added CSP header

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,14 +9,14 @@ const nextConfig = {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://lh3.googleusercontent.com/',
-          },
           // {
-          //   key: 'Content-Security-Policy',
-          //   value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+          //   key: 'X-Frame-Options',
+          //   value: 'ALLOW-FROM https://lh3.googleusercontent.com/',
           // },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; style-src 'self'; frame-ancestors https://lh3.googleusercontent.com/; form-action 'self'",
+          },
         ],
       },
     ];
