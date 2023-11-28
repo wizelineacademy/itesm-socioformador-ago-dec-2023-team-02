2516da629cf947ca1c25adf09792b05dee180a4b
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-11-28 14:17:54 -0600

    style: 💄 Styled group header adn body padding

--- apps/web/components/admin/groupBody/molecules/group-header.tsx
+++ apps/web/components/admin/groupBody/molecules/group-header.tsx
@@ -44,12 +44,13 @@ export function GroupHeader({
   }
 
   return (
-    <div className="flex flex-wrap items-start justify-between mb-2 sm:ml-5 mt-0 pt-0">
+    <div className="flex flex-wrap items-start justify-between mb-2 mt-2 pt-0">
       {/* Adjust the order of items for XS screens */}
-      <div className="w-full sm:w-1/2 order-2 sm:order-1 text-start">
-        <button className="flex flex-row justify-center items-center gap-2" onClick={handleGroupNameClick} type="button">
-        <LuInfo size="1rem"/>
+      <div className="w-full sm:w-1/2 order-2 sm:order-1 text-start sm:mt-10">
+        <button className="flex flex-row justify-center items-center gap-3" onClick={handleGroupNameClick} type="button">
+        
           <h3 className="text-3xl font-bold">{groupData.name}</h3>
+          <LuInfo size="1rem"/>
         </button>
 
         {/* Placeholder for credits information below the group name */}
